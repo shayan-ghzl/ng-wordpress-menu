5d475c1db53b79562034c7625799418900c04c0f
Shayan Khan <shayan97.tti@gmail.com>
2023-03-14 16:37:19 +0330

    project initialized and admin menu stylized

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SidebarComponent } from './layout/sidebar/sidebar.component';
+import { ContentComponent } from './layout/content/content.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SidebarComponent,
+    ContentComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/layout/sidebar/sidebar.component.ts
@@ -0,0 +1,30 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+})
+export class SidebarComponent {
+  constructor() {
+    // document.body.classList.add('ltr');
+    // document.body.classList.add('sticky-menu');
+    // document.body.classList.add('folded');
+    // document.body.classList.add('no-js');
+    // document.body.classList.add('auto-fold');
+    // document.body.classList.add('auto-fold');
+    // document.body.classList.add('upper-space');
+    // document.body.classList.add('focus-off');
+    // document.body.classList.add('focus-on');
+    // document.body.classList.add('wp-responsive-open');
+    // document.body.classList.add('is-fullscreen-mode');
+    // document.body.classList.add('sub-open');
+    // document.body.classList.add('opensub');
+    // document.body.classList.add('menu-over-content');
+  }
+
+  stayOpen(){}
+  closeIt(){}
+
+}
